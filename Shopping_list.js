7425de14271723db84a63c40ae9c050cb921a137
Manny LEE <manlee@MacBook-Air.local>
2021-07-15 14:36:44 -0700

    v1 shopping list

---
+++ Shopping_list.js
@@ -0,0 +1,8 @@
+        //Shopping List
+
+var myList =[
+    ['Chocolate Bars',4],['Banana', 10],['Cookies',12],['Milk',2],['Strawberries',12], ['Blackberries',12]
+    ]
+console.log(myList)
+
+console.log(`Get ${myList[0][0,1]} ${myList[0][0,0]}`)
