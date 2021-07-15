0afec2c6e3c106f113bbff3351cbb01fa90c570d
Manny LEE <manlee@MacBook-Air.local>
2021-07-15 14:40:04 -0700

    v1 world blanks

---
+++ Word_blanks.js
@@ -0,0 +1,10 @@
+         //Word Blanks
+
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+ var myWordBlanks ="The "+ myAdjective +" "+myNoun+" "+myVerb+" "+myAdverb;
+ 
+ console.log(myWordBlanks)
