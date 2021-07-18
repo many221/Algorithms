e42da1f80620df40ffdb5943f0f028d46b090e90
Manny LEE <many221@gmail.com>
2021-07-18 07:14:00 -0700

    {commit bash script}

--- daily_commits.sh
+++ daily_commits.sh
@@ -1,18 +1,23 @@
 #!/bin/bash 
- # runs add and commit for 3 files GitHub
-
-echo Please chose 3 files  and message to comimitto add and commit
-
-echo file 1
-
-read  var1 
  
-echo message 1
+# runs add and commit files GitHub
+
+#echo Please chose a file and message to comimitto add and commit 
+
+select file in "$search_dir"./*
+
+do
+
+echo You have chosen $file to add and commit.
+ 
+echo What would you like to add as a note
 
 read m1
 
-git add $var1
+git add $file
 
-git commit $var1 -m “ $m1”
+git commit $file -m "{$m1}"
 
-echo Congratulations you have /done your daily commits
+echo Congratulations you have "done" your daily commits
+
+done
