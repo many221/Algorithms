7c998b2b5705470079e5b67cb2713ef0733e4aef
Manny LEE <many221@gmail.com>
2021-07-24 00:44:22 -0700

    {reverse test function}

---
+++ revfunc.js
@@ -0,0 +1,17 @@
+function rever(word) {
+    
+    let rev = []
+
+    for (let i = word.length; 0 >= word.length; i++) {
+        
+        rev.push(word[i])
+        
+    }
+
+    
+
+    console.log(rev.join());
+}
+
+
+rever("Hello")
