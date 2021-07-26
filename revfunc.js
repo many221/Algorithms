05c32209bcd468c6b26ed181663a60501b35904e
Manny LEE <many221@gmail.com>
2021-07-25 19:43:18 -0700

    {2}

--- revfunc.js
+++ revfunc.js
@@ -5,12 +5,12 @@ function rever(word) {
     for (let i = word.length; 0 >= word.length; i++) {
         
         rev.push(word[i])
-        
+       console.log(word[i]); 
     }
 
     
 
-    console.log(rev.join());
+    console.log(rev);
 }
 
 
